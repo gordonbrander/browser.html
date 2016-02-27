098055c2a3e6db5f0f94ba7176d7fb13a21a5482
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-02-26 17:37:40 -0800

    Move pallets to pallet.json
    Add pallet for Medium

--- src/browser/pallet.js
+++ src/browser/pallet.js
@@ -9,25 +9,8 @@
 import tinycolor from 'tinycolor2';
 import {Effects, Task} from 'reflex';
 import * as URI from '../common/url-helper';
-
-// Hand-curated themes for popular websites.
-const curated = {
-  'facebook.com': {background: '#3a5795', foreground: '#fff'},
-  'sina.com.cn': {background: '#ff8500', foreground: '#fff'},
-  'reddit.com': {background: '#f0f0f0', foreground: '#336699'},
-  'instagram.com': {background: '#125688', foreground: '#fff'},
-  'imgur.com': {background: '#2b2b2b', foreground: '#fff'},
-  'cnn.com': {background: '#0c0c0c', foreground: '#fff'},
-  'slideshare.net': {background: '#313131', foreground: '#fff'},
-  'deviantart.com': {background: '#475c4d', foreground: '#fff'},
-  'soundcloud.com': {background: '#333333', foreground: '#FF5500'},
-  'mashable.com': {background: '#00aeef', foreground: '#fff'},
-  'daringfireball.net': {background: '#4a525a', foreground: '#fff'},
-  'firewatchgame.com': {background: '#2d102b', foreground: '#ef4338'},
-  'whatliesbelow.com': {background: '#74888b', foreground: '#fff'},
-  'supertimeforce.com': {background: '#051224', foreground: '#2ebcec'},
-  'github.com': {background: 'rgb(245, 245, 245)', foreground: 'rgb(51, 51, 51)'},
-};
+// Import hand-curated color pallets for popular websites.
+import curated from '../../pallet.json';
 
 // Calculate the distance from white, returning a boolean.
 // This is a pretty primitive check.
