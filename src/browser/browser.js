5732edf1fd99f29b400e4c8739f3bb8d960d2e0b
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-03-17 12:37:13 -0700

    Remove initWithWebView
    createWebView takes care of opening the about:newtab webview already.

--- src/browser/browser.js
+++ src/browser/browser.js
@@ -38,14 +38,7 @@ import {onWindow} from "driver";
 export const init/*:type.init*/ = () => {
   const [devtools, devtoolsFx] = Devtools.init({isActive: Config.devtools});
   const [shell, shellFx] = Shell.init();
-  const [webViews, webViewsFx] = WebViews.initWithWebView
-    // @TODO in future we should read about:newtab closer to the iframe
-    ( { uri: URI.read('about:newtab')
-      , name: ''
-      , features: ''
-      , inBackground: false
-      }
-    );
+  const [webViews, webViewsFx] = WebViews.init();
   const [sidebar, sidebarFx] = Sidebar.init();
 
   const model =
